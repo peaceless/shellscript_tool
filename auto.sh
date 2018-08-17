48d7ae31f16f6f3ffdfb4fd25dab451bc32237de
OriginalS <tengzhou989@gmail.com>
2018-08-17 21:00:57 +0800

    update

--- auto.sh
+++ auto.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-cd /Users/originals/Boostnote
-count=`git status -s | wc -l`
-if [ $count -gt 0 ]; then
-    echo "something in buffer"
-    git add *
-    git commit -m "update"
-    if [ $? -eq 0 ]; then
-        git push origin master
-    else
-        echo untrack file
-        git status
+project=("/User/originals/Boostnote" "/Users/originals/.myscript")
+function update() {
+    for pro in $project; do
+        cd $pro
+        count=`git status -s | wc -l`
+        if [ $count -gt 0 ]; then
+        echo "something in buffer"
+        git add *
+        git commit -m "update"
+        if [ $? -eq 0 ]; then
+            git push origin master
+        else
+            echo untrack file
+            git status
+        fi
+        echo update $pro
     fi
-fi
-echo finish
+    done
+}
+update
